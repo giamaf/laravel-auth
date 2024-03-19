4c2b8c4bea1e119142c777d35e7f0d0eca524200
giamaf <gianluca.maffucci@hotmail.com>
2024-03-19 17:11:36 +0100

    make model + migration + factory + resController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\HomeController as AdminHomeController;
 
@@ -17,9 +18,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rotta per gli utenti visitatori
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+// Rotte per l'amministratore
+
+//* OPZIONE 1 (no grouping)
+// Route::get('/admin', AdminHomeController::class)->middleware(['auth'])->name('admin.home');
+// Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index')->middleware('auth');
+// Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create')->middleware('auth');
+// Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show')->middleware('auth');
+// Route::post('/admin/projects', [ProjectController::class, 'store'])->name('admin.projects.store')->middleware('auth');
+// Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit')->middleware('auth');
+// Route::put('/admin/projects/{project}', [ProjectController::class, 'update'])->name('admin.projects.update')->middleware('auth');
+// Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy')->middleware('auth');
+
+//* OPZIONE 2 (grouping)
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function(){
+
+    //# ROTTA ADMIN HOME
+    Route::get('', AdminHomeController::class)->middleware(['auth'])->name('.home');
+
+    //# ROTTE ADMIN PROJECTS - OPZIONE 1
+    // Route::get('/projects', [ProjectController::class, 'index'])->name('.projects.index');
+    // Route::get('/projects/create', [ProjectController::class, 'create'])->name('.projects.create');
+    // Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('.projects.show');
+    // Route::post('/projects', [ProjectController::class, 'store'])->name('.projects.store');
+    // Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('.projects.edit');
+    // Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('.projects.update');
+    // Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('.projects.destroy');
+
+    //# ROTTE ADMIN PROJECTS - OPZIONE 2
+    //! RIASSUNTO DA RIGA 41 A RIGA 47 CON UNA SOLA ROTTA CHE SOTTOBANCO GESTISCE LE ROTTE DI CUI SOPRA
+    //! PER VERIFICARE -> php artisan route:list
+    Route::resource('projects', ProjectController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
