f234d47425df2edbb8d2a0343979be0a5461f493
giamaf <gianluca.maffucci@hotmail.com>
2024-03-25 16:36:38 +0100

    added customized factory file image name

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
@@ -20,11 +21,18 @@ class ProjectFactory extends Factory
 
         $name = fake()->text(20);
 
+        $slug = Str::slug($name);
+
+        $image_file = fake()->image(null, 250, 250);
+
+        // Creiamo una cartella ogni qualvolta refreshiamo il db
+        Storage::makeDirectory('project_images');
+
         return [
             'name' => $name,
-            'slug' => Str::slug($name),
+            'slug' => $slug,
             'content' => fake()->paragraphs(1, true),
-            'image' => fake()->imageUrl(250, 250, true),
+            'image' => Storage::putFileAs('project_images', $image_file, "$slug.png"),
             'is_completed' => fake()->boolean(),
         ];
     }
